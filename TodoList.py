c26db141cfc9c8368afc317e51e3d41d54a4bd66
Axel Gomez <axelrg09@gmail.com>
2024-12-02 17:21:44 -0300

    refactor code and add functions to save and load tasks

--- TodoList.py
+++ TodoList.py
@@ -1,7 +1,7 @@
 #To-Do List project
+from json import JSONDecodeError
 from operator import index
 import json
-
 
 
 #Variables
@@ -11,7 +11,7 @@ Tasks =[]
 #Functions
 def createTask():
     follow = 1
-
+#Create a bucle to repit while "do yo want add new task" its say "okey see you later" or similar thinks
     while follow == 1:
         newTask = input("Enter your new Task!: ")
         Task.append(newTask) #add newTask in a new task list
@@ -22,9 +22,10 @@ def createTask():
 
         Task.clear() #clear task list for add new task list in Tasks (it contain all tasks)
 
-        follow = int(input("\nDo you want continue?: \nPress [1] for YES\nPress [0] for NO: "))
+        follow = int(input("\nDo You need add a new task?: \nPress [1] for YES\nPress [0] for NO: "))
         if follow != 1:
             break
+
 
 def showTasks():
     y = 1
@@ -34,22 +35,27 @@ def showTasks():
         print("Estate: ",t[1])
         y+=1
 
+
 def deleteTask():
     print("\n|-- We recommending look yours task first before delete something --|")
     showTasks()
     selector = int(input("\n|-- Which one you wan delete? --|\nEnter number of task you wan delete: "))
     Tasks.pop(index(selector-1))
 
+
 def changeStatus():
+
+    # add solution in case of dont have anyone list. e.g: 0 tasks
     print("\n|-- We recommending look yours task first before delete something --|")
     showTasks()
     selector = int(input("\n|-- Which task you wan change its Status? --|\nEnter number of task you wan change status: "))
     Tasks[selector-1][1] = input("What is the new status of this stask: ")
 
-"""
+
 def saveTasks():
     with open("tasks.json", "w") as file:
         json.dump(Tasks, file, indent=4)
+
 
 def loadTasks():
     try:
@@ -60,8 +66,10 @@ def loadTasks():
     except json.JSONDecodeError:
         print("Error: Damaged file or not JSON valid ")
         return[]
+
+
 #MAIN
-tasks = loadTasks()"""
+tasks = loadTasks()
 
 showTasks()
 
